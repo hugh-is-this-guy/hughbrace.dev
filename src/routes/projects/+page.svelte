e8427087e2538e55eb7053c11948fd8ea03a9d80
Hugh <hughbrace@gmail.com>
2024-03-30 21:14:58 -0300

    Animate projects to make people click

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { onMount, onDestroy } from 'svelte';
+
   export let data
 
   const imports = import.meta.glob('./images/*/1.jpg', {
@@ -20,7 +22,31 @@
     pictures[slug] = image;
   }
 
+  let images = []
+
   $: ({ projects } = data)
+
+  let interval;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      images[Math.floor(Math.random() * images.length)].animate([
+        { transform: 'rotate(0deg)' },
+        { transform: 'rotate(4deg)' },
+        { transform: 'rotate(0deg)' },
+        { transform: 'rotate(4deg)' },
+        { transform: 'rotate(0deg)' },
+      ], {
+        duration: 500,
+        iterations: 1
+      });
+      }, 5000);
+  })
+
+  onDestroy(() => {
+    clearInterval(interval);
+  })
+
 </script>
 
 <div class="py-12">
@@ -31,6 +57,7 @@
 
       <a href="/projects/{project.slug}" class="flex flex-col items-center justify-between">
         <enhanced:img
+          bind:this={images[i]}
           src={pictures[project.slug]}
           alt={project.title}
           class="transition hover:rotate-3 mb-4"
